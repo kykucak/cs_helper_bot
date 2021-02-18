76d20e744d9a06bae7d74cfd07ee4b6c8192e3ee
kykucak <kykucak@gmail.com>
2021-02-18 22:30:07 +0200

    Create tables

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String, nullable=False)
     link = db.Column(db.String, nullable=False)
+db.create_all()
 
 
 def get_date(date: int):
